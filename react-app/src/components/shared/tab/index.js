aa325bea4f8bf97b59efc8260ff2eb70563847e8
Mounish-Kumar <vmounishkumar@gmail.com>
2020-12-05 14:20:35 +0530

    Created structure

---
+++ react-app/src/components/shared/tab/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Tab extends Component {
+  state = {};
+  render() {
+    return <React.Fragment></React.Fragment>;
+  }
+}
+
+export default Tab;
