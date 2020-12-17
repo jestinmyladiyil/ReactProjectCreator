ae3788cfef568099531dc49231c1765b9d31f7f5
Mathew Jestin <jestin.mathew@centrico.tech>
2020-12-17 10:38:12 +0530

    Pagination: component implementation and usage documentation

--- react-app/src/components/shared/pagination/index.js
+++ react-app/src/components/shared/pagination/index.js
@@ -1,9 +1,114 @@
 import React, { Component } from "react";
 
 class Pagination extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageSize: 10,
+      startIndex: 1,
+      endIndex: 10,
+      rowCountDifference: props.rowCountDifference,
+    };
+  }
+
+  validateIndex = (index) => index >= 0 && index <= this.props.totalItems;
+
+  changeRowsPerPage = (event) => {
+    this.setState(
+      {
+        pageSize: Number(event.target.value),
+      },
+      () => {
+        this.navigatePage(1);
+      }
+    );
+  };
+
+  navigatePage = (pageNumber) => {
+    this.setState((prevState) => ({
+      startIndex: (pageNumber - 1) * prevState.pageSize + 1,
+      endIndex: Math.min(
+        pageNumber * prevState.pageSize,
+        this.props.totalItems
+      ),
+    }));
+    this.props.onPageChange(pageNumber);
+  };
+
   render() {
-    return <React.Fragment></React.Fragment>;
+    const { totalItems, currentPage } = this.props;
+    const {
+      pageSize,
+      startIndex,
+      endIndex,
+      rowCountDifference = 5,
+    } = this.state;
+    const pagesCount = Math.ceil(totalItems / pageSize);
+    let rowCountsList = [];
+    for (var counter = 1; counter < 20; counter++) {
+      rowCountsList.push(counter * rowCountDifference);
+    }
+    return (
+      <React.Fragment>
+        <div className="pagination">
+          <span>Rows per page: </span>
+          <span>
+            <select
+              id="rowCountSelector"
+              onChange={this.changeRowsPerPage}
+              className="page-link"
+              defaultValue="10"
+            >
+              {rowCountsList.map((rowCountValue) => (
+                <option value={rowCountValue}>{rowCountValue}</option>
+              ))}
+            </select>
+          </span>
+          <span className="page-item">
+            Page {currentPage} of {pagesCount}
+          </span>
+          <span>
+            Displaying {startIndex} to {endIndex} items of {totalItems}
+          </span>
+          {pagesCount !== 1 ? (
+            <div>
+              <a
+                className="page-link"
+                onClick={() => currentPage !== 1 && this.navigatePage(1)}
+              >
+                &laquo;
+              </a>
+              <a
+                className="page-link"
+                onClick={() =>
+                  currentPage !== 1 && this.navigatePage(currentPage - 1)
+                }
+              >
+                &lt;
+              </a>
+              <a className="page-item">{currentPage}</a>
+              <a
+                className="page-link"
+                onClick={() =>
+                  currentPage !== pagesCount &&
+                  this.navigatePage(currentPage + 1)
+                }
+              >
+                &gt;
+              </a>
+              <a
+                className="page-link"
+                onClick={() =>
+                  currentPage !== pagesCount && this.navigatePage(pagesCount)
+                }
+              >
+                &raquo;
+              </a>
+            </div>
+          ) : null}
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
--- react-app/src/components/shared/pagination/index.usage.js
+++ react-app/src/components/shared/pagination/index.usage.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 import CodeHighlighter from "../code-highlighter";
+import Pagination from ".";
 
 class PaginationUsage extends Component {
-  code = (
-    <div>Your component usage should come here ..........................</div>
-  );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      totalItems: 95,
+      currentPage: 1,
+    };
+  }
+
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
+  code = (<div></div>);
 
   render() {
     return (
@@ -13,10 +25,33 @@ class PaginationUsage extends Component {
         <div>Add some description here</div>
         <br />
         <h3>Preview</h3>
-        <div>{this.code}</div>
+        <div>
+          <h4>Items displayed from page {this.state.currentPage}</h4>
+          <Pagination
+            totalItems={this.state.totalItems}
+            currentPage={this.state.currentPage}
+            onPageChange={this.handlePageChange}
+          ></Pagination>
+        </div>
         <br />
         <h3>Usage</h3>
-        <CodeHighlighter>{this.code}</CodeHighlighter>
+        <br />
+        Component configuration:
+        <CodeHighlighter language="js">
+          {`<Pagination
+            totalItems=95 // Total number of items
+            currentPage=1 // Initial page to be displayed
+            rowCountDifference=5 // Differences between values in 'Rows per page' dropdown. (Optional : 5 is default)
+            onPageChange={this.handlePageChange} // Method to handle page navigation
+></Pagination>`}
+        </CodeHighlighter>
+        <br />
+        Page navigation method :
+        <CodeHighlighter language="js">
+          {`handlePageChange = (page) => {
+    //Logic goes here...
+  };`}
+        </CodeHighlighter>
       </React.Fragment>
     );
   }
